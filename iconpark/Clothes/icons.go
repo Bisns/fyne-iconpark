ad4870608778ae55c8011b3ccaf883994b675669
Bisns <b@bisn.cn>
2022-09-22 14:37:52 +0800

    update

--- iconpark/Clothes/icons.go
+++ iconpark/Clothes/icons.go
@@ -73,7 +73,7 @@ var (
 	swimsuit             *theme.ThemedResource
 	tshirt               *theme.ThemedResource
 	thesingleshoulderbag *theme.ThemedResource
-	theme_               *theme.ThemedResource
+	theme                *theme.ThemedResource
 	threedglasses        *theme.ThemedResource
 	trousersbellbottoms  *theme.ThemedResource
 	trunk                *theme.ThemedResource
@@ -148,7 +148,7 @@ func init() {
 	swimsuit = theme.NewThemedResource(resourceSwimsuitSvg)
 	tshirt = theme.NewThemedResource(resourceTShirtSvg)
 	thesingleshoulderbag = theme.NewThemedResource(resourceTheSingleShoulderBagSvg)
-	theme_ = theme.NewThemedResource(resourceThemeSvg)
+	theme = theme.NewThemedResource(resourceThemeSvg)
 	threedglasses = theme.NewThemedResource(resourceThreeDGlassesSvg)
 	trousersbellbottoms = theme.NewThemedResource(resourceTrousersBellBottomsSvg)
 	trunk = theme.NewThemedResource(resourceTrunkSvg)
@@ -484,7 +484,7 @@ func ThesingleshoulderbagIcon() fyne.Resource {
 
 // ThemeIcon returns Theme icon resource
 func ThemeIcon() fyne.Resource {
-	return theme_
+	return theme
 }
 
 // ThreedglassesIcon returns Threedglasses icon resource
